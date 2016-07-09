6737abb3243d1463aaa36839b5b9698b3967e9ed
Estheroy <xuouyang@ucsd.edu>
2016-07-08 18:57:01 -0700

    update gait-analysis

--- gaitSpeedTest.m
+++ gaitSpeedTest.m
@@ -126,7 +126,7 @@ for k=1:count
     seg_fullname = fullfile('test_data','seg_images',seg_filename);
     imwrite(seg_image,seg_fullname);
 end
-%% Pixel difference domain 
+%% Pixel difference domain | set threshold to 15
 
 %count = i-1;
 for i=1:count
@@ -151,6 +151,214 @@ for i=1:count
 end
 
 
+
+%% clear noise test
+i = 66;
+imageIndexName = [sprintf('%03d',i) '.jpg'];
+img = double(imread(fullfile('test_data','diff_threshold',imageIndexName)));
+
+rec1 = img(1:540,1:320);
+rec2 = img(1:540,321:640);
+rec3 = img(1:540,641:960);
+rec4 = img(1:540,961:1280);
+rec5 = img(1:540,1281:1600);
+rec6 = img(1:540,1601:1920);
+
+
+rec7 = img(541:1080,1:320);
+rec8 = img(541:1080,321:640);
+rec9 = img(541:1080,641:960);
+rec10 = img(541:1080,961:1280);
+rec11 = img(541:1080,1281:1600);
+rec12 = img(541:1080,1601:1920);
+
+rec_arr = [rec1 rec2 rec3 rec4 rec5 rec6 rec7 rec8 rec9 rec10 rec11 rec12];
+col1 = [1, 321, 641, 961, 1281, 1601, 1921, 2241, 2561, 2881, 3201, 3521];
+col2 = [320,640,960, 1280,1600, 1920, 2240, 2560, 2880, 3200, 3520, 3840];
+row1 = [1, 541];
+row2 = [540,1080];
+re_img = zeros(540, 3840);
+
+for i=1:12
+    col_ind_start = col1(i);
+    col_ind_end = col2(i);
+    
+    sectioned_img = rec_arr(:,col_ind_start:col_ind_end);
+    summ = sum(sum(sectioned_img))
+    
+    if summ > 100000
+        re_img(:,col_ind_start:col_ind_end) = rec_arr(:,col_ind_start:col_ind_end);
+    end
+end
+
+reg_img = [re_img(:,1:1920); re_img(:,1921:3840)];
+
+figure;
+imshow(reg_img);
+
+%% clear noise and write the image to the file
+
+count = 1207;
+for i=1:count
+   
+   % get the previous and next image
+   imageIndexName_prev = [sprintf('%03d',i) '.jpg'];
+   img_prev = double(imread(fullfile('test_data','diff_threshold',imageIndexName_prev)));
+   
+   result_img = clear_noise(img_prev);
+   
+   diff_filename = [sprintf('%03d',i) '.jpg'];
+   diff_fullname = fullfile('test_data','diff_threshold_without_noise',diff_filename);
+   imwrite(uint8(result_img),diff_fullname);
+end
+
+%%
+
+%% clear noise and write the image to the file
+
+count = 1207;
+%for i=1:count
+   i = 200;
+   % get the previous and next image
+   imageIndexName_prev = [sprintf('%03d',i) '.jpg'];
+   img_prev = double(imread(fullfile('test_data','diff_threshold',imageIndexName_prev)));
+   
+   result_img = clear_noise(img_prev);
+   
+    rec1 = sum(sum(result_img(1:540,1:320)));
+    rec2 = sum(sum(result_img(1:540,321:640)));
+    rec3 = sum(sum(result_img(1:540,641:960)));
+    rec4 = sum(sum(result_img(1:540,961:1280)));
+    rec5 = sum(sum(result_img(1:540,1281:1600)));
+    rec6 = sum(sum(result_img(1:540,1601:1920)));
+
+    rec7 = sum(sum(result_img(541:1080,1:320)));
+    rec8 = sum(sum(result_img(541:1080,321:640)));
+    rec9 = sum(sum(result_img(541:1080,641:960)));
+    rec10 = sum(sum(result_img(541:1080,961:1280)));
+    rec11 = sum(sum(result_img(541:1080,1281:1600)));
+    rec12 = sum(sum(result_img(541:1080,1601:1920)));
+
+    rec_arr = [rec1 rec2 rec3 rec4 rec5 rec6 rec7 rec8 rec9 rec10 rec11 rec12];
+
+    [val1, position1] = max(rec_arr);
+    rec_arr(position1) = 0;
+    [val2, position2] = max(rec_arr);
+    rec_arr(position2) = 0;
+    [val3, position3] = max(rec_arr);
+    rec_arr(position3) = 0;
+    [val4, position4] = max(rec_arr);
+    rec_arr(position4) = 0;
+    [val5, position5] = max(rec_arr);
+    rec_arr(position5) = 0;
+    [val6, position6] = max(rec_arr);
+    
+    position1
+    position2
+    position3
+    position4
+    position5
+    position6
+    
+    result = zeros(1080, 1920);
+    
+    % get two rectangle and copy the rectangle into the result image
+    result(1:540,961:1280) = result_img(1:540,961:1280);
+    %result(1:540,1281:1600) = result_img(1:540,1281:1600);
+    %result(541:1080,1281:1600) = result_img(541:1080,1281:1600);
+    %result(541:1080,1601:1920) = result_img(541:1080,1601:1920);
+    %result(1:540,1:320) = result_img(1:540,1:320);
+    %result(541:1080,1:320) = result_img(541:1080,1:320);
+    
+    result(541:1080,961:1280) = result_img(541:1080,961:1280);
+
+    imshow(uint8(result));
+    
+%%
+count = 1207;
+%for i=1:count
+   i = 200;
+   % get the previous and next image
+   imageIndexName_prev = [sprintf('%03d',i) '.jpg'];
+   img_prev = double(imread(fullfile('test_data','diff_threshold',imageIndexName_prev)));
+   
+   result_img = clear_noise(img_prev);
+   
+    rec1 = sum(sum(result_img(1:540,1:320)));
+    rec2 = sum(sum(result_img(1:540,321:640)));
+    rec3 = sum(sum(result_img(1:540,641:960)));
+    rec4 = sum(sum(result_img(1:540,961:1280)));
+    rec5 = sum(sum(result_img(1:540,1281:1600)));
+    rec6 = sum(sum(result_img(1:540,1601:1920)));
+
+    rec7 = sum(sum(result_img(541:1080,1:320)));
+    rec8 = sum(sum(result_img(541:1080,321:640)));
+    rec9 = sum(sum(result_img(541:1080,641:960)));
+    rec10 = sum(sum(result_img(541:1080,961:1280)));
+    rec11 = sum(sum(result_img(541:1080,1281:1600)));
+    rec12 = sum(sum(result_img(541:1080,1601:1920)));
+
+    rec_arr = [rec1 rec2 rec3 rec4 rec5 rec6 rec7 rec8 rec9 rec10 rec11 rec12];
+
+    [val1, position1] = max(rec_arr);
+    rec_arr(position1) = 0;
+    [val2, position2] = max(rec_arr);
+    rec_arr(position2) = 0;
+    [val3, position3] = max(rec_arr);
+    rec_arr(position3) = 0;
+    [val4, position4] = max(rec_arr);
+    rec_arr(position4) = 0;
+    [val5, position5] = max(rec_arr);
+    rec_arr(position5) = 0;
+    [val6, position6] = max(rec_arr);
+    
+    position1
+    position2
+    position3
+    position4
+    position5
+    position6
+    
+    result = zeros(1080, 1920);
+    
+    % get two rectangle and copy the rectangle into the result image
+    result(1:540,961:1280) = result_img(1:540,961:1280);
+    %result(1:540,1281:1600) = result_img(1:540,1281:1600);
+    %result(541:1080,1281:1600) = result_img(541:1080,1281:1600);
+    %result(541:1080,1601:1920) = result_img(541:1080,1601:1920);
+    %result(1:540,1:320) = result_img(1:540,1:320);
+    %result(541:1080,1:320) = result_img(541:1080,1:320);
+    
+    result(541:1080,961:1280) = result_img(541:1080,961:1280);
+
+    imshow(uint8(result));
+    
+ 
+
+%% Determine the start and the end of the walk by the intensity of image
+
+count = 1207
+image_intensity= zeros(1,count);
+
+for i=1:count
+   
+   % get the previous and next image
+   imageIndexName = [sprintf('%03d',i) '.jpg'];
+   img = double(imread(fullfile('test_data','diff_threshold_without_noise',imageIndexName)));
+   image_intensity(i) = sum(sum(img));
+    
+end
+
+figure;
+x = 1:count;
+plot (x, image_intensity);
+hold on;
+plot ([1, count], [image_intensity(1), image_intensity(count)],'r^');
+hold off;
+axis tight;
+ylabel('White Intensity');
+xlabel('Time elapsed in 1 frame');
+title('Gait Test Event Intensty Over Time');
 %% Data Segmentation | Edge detection
 i = 1;
 
@@ -196,23 +404,23 @@ imshow(final_img/255);
 
 %% Create output video file from processes image frames
 
-imageNames = dir(fullfile('test_data','diff_images','*.jpg'));
+imageNames = dir(fullfile('test_data','diff_threshold_without_noise','*.jpg'));
 imageNames = {imageNames.name}';
 
-outputVideo = VideoWriter(fullfile('test_data/output','diff_test.avi'));
+outputVideo = VideoWriter(fullfile('test_data/output','diff_threshold_without_noise.avi'));
 outputVideo.FrameRate = inputRGBVideo.FrameRate;
 open(outputVideo)
 
 %for i = 1:length(imageNames)
-for i = 1:1005
+for i = 1:1207
 
    imageIndexName = [sprintf('%03d',i) '.jpg'];
    %img = imread(fullfile('test_data','yuv_images',imageNames{i}));
-   img = imread(fullfile('test_data','diff_images',imageIndexName));
-   writeVideo(outputVideo,img)
+   img = imread(fullfile('test_data','diff_threshold_without_noise',imageIndexName));
+   writeVideo(outputVideo,img);
 end
 
-close(outputVideo)
+close(outputVideo);
 
 %% binary data segmentation
 
@@ -244,3 +452,49 @@ BW2 = edge(I,'canny');
 figure;
 imshowpair(BW1,BW2,'montage')
 title('Sobel Filter                                   Canny Filter');
+
+
+%% graph the white intensity over frame
+ 
+count = 1207;
+S = zeros(1,count);
+for i=1:count
+    
+    % get the previous and next image
+    imageIndexName_curr = [sprintf('%03d',i) '.jpg'];
+    img_curr = double(imread(fullfile('test_data','diff_threshold',imageIndexName_curr)));
+    img = clear_noise(img_curr);
+    S(i) = sum(sum(img));
+end 
+
+figure
+x = 1:size(S, 2);
+plot (x, S);
+hold on;
+plot ([1, count], [S(1), S(count)],'r^');
+hold off;
+axis tight;
+ylabel('White Intensity');
+xlabel('Time elapsed in 1 frame');
+title('Gait Test Event Intensty Over Time');
+%% graph the white intensity over frame
+ 
+count = 1207;
+S = zeros(1,count);
+for i=1:count
+    % get the previous and next image
+    imageIndexName_curr = [sprintf('%03d',i) '.jpg'];
+    img_curr = double(imread(fullfile('test_data','diff_threshold_without_noise',imageIndexName_curr)));
+    S(i) = sum(sum(img));
+end
+ 
+figure
+x = 1:size(S, 2);
+plot (x, S);
+hold on;
+plot ([1, count], [S(1), S(count)],'r^');
+hold off;
+axis tight;
+ylabel('White Intensity');
+xlabel('Time elapsed in 1 frame');
+title('Gait Test Event Intensty Over Time');
