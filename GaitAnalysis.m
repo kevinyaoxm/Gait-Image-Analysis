0535b224534038299b05080d01a683f26a61ca2d
Estheroy <xuouyang@ucsd.edu>
2016-07-22 00:07:42 -0700

    update box

--- GaitAnalysis.m
+++ GaitAnalysis.m
@@ -116,122 +116,128 @@ end
 
 %% Calculate the position of the person by the original RGB video
 
-i = 150;
-imageIndexName = [sprintf('%03d',i) '.jpg'];
-img = double(imread(fullfile('test_data','diff_mf_threshold2',imageIndexName)));
+count = 1207;
+for itera=1:count
+    imageIndexName = [sprintf('%03d',itera) '.jpg'];
+    img = double(imread(fullfile('test_data','diff_mf_threshold3',imageIndexName)));
 
-figure;
-[pixelCount, grayLevels] = imhist(img);
-bar(pixelCount);
-title('Histogram of original image');
-xlim([0 grayLevels(end)]); % Scale x axis manually.
+%     figure;
+%     [pixelCount, grayLevels] = imhist(img);
+%     bar(pixelCount);
+%     title('Histogram of original image');
+%     xlim([0 grayLevels(end)]); % Scale x axis manually.
 
-thresholdValue = 240;
+    thresholdValue = 240;
 
-line([thresholdValue, thresholdValue], ylim, 'Color', 'r');
-img = imfill(img);
-binaryImage = imbinarize(img, 80);
-binaryImage = imfill(binaryImage, 'holes');
-% imshow(binaryImage, []);
+    line([thresholdValue, thresholdValue], ylim, 'Color', 'r');
+    img = imfill(img);
+    binaryImage = imbinarize(img, 80);
+    binaryImage = imfill(binaryImage, 'holes');
+    % imshow(binaryImage, []);
 
-horizontal_sum_image = sum(binaryImage,1);
-plot(horizontal_sum_image);
+    horizontal_sum_image = sum(binaryImage,1);
+    % plot(horizontal_sum_image);
 
-binaryImage(:,1250:1920) = 0;
-img(:,1250:1920) = 0;
+    binaryImage(:,1250:1920) = 0;
+    img(:,1250:1920) = 0;
 
-imshow(binaryImage, []);
+    %imshow(binaryImage, []);
 
-horizontal_sum = sum(img,1);
-vertical_sum = sum(img,2);
+    horizontal_sum = sum(img,1);
+    vertical_sum = sum(img,2);
 
 
-% Find the start_index and end_index of horizontal_sum
-peak_threshold = 2000;
-horizontal_start_index = 1;
-for i=1:length(horizontal_sum)-1; % Loop through vector intensityOverTime - 1 to avoid accessing past the vector
-    difference=abs(horizontal_sum(i+1)-horizontal_sum(i)); % calculate difference between neighboring intensities
-    if difference > peak_threshold% If statement to determine start time index
-        horizontal_start_index=i;
-        break;
+    % Find the start_index and end_index of horizontal_sum
+    peak_threshold = 2000;
+    horizontal_start_index = 1;
+    for i=1:length(horizontal_sum)-1; % Loop through vector intensityOverTime - 1 to avoid accessing past the vector
+        difference=abs(horizontal_sum(i+1)-horizontal_sum(i)); % calculate difference between neighboring intensities
+        if difference > peak_threshold% If statement to determine start time index
+            horizontal_start_index=i;
+            break;
+        end
     end
-end
 
-peak_threshold_rev = 2000;
+    peak_threshold_rev = 2000;
 
-horizontal_stop_index = 1;
-for i=length(horizontal_sum):-1:2; % Loop through vector intensityOverTime - 1 to avoid accessing past the vector
-    difference = abs(horizontal_sum(i)-horizontal_sum(i-1)) % calculate difference between neighboring intensities
-    if difference > peak_threshold_rev  % If statement to determine start time index
-        horizontal_stop_index=i;
-        break;
+    horizontal_stop_index = 1;
+    for i=length(horizontal_sum):-1:2; % Loop through vector intensityOverTime - 1 to avoid accessing past the vector
+        difference = abs(horizontal_sum(i)-horizontal_sum(i-1)) % calculate difference between neighboring intensities
+        if difference > peak_threshold_rev  % If statement to determine start time index
+            horizontal_stop_index=i;
+            break;
+        end
     end
-end
 
-subplot(1,2,1);
-x = 1:size(horizontal_sum, 2);
-plot (x, horizontal_sum);
-hold on;
-plot ([horizontal_start_index, horizontal_stop_index], [horizontal_sum(horizontal_start_index), horizontal_sum(horizontal_stop_index)],'r^');
-axis tight;
-ylabel('Event Intensity');
-xlabel('Pixel Coordinate');
-title('Distribution of event intensity - horizontal sum');
-hold off;
+    % subplot(1,2,1);
+    % x = 1:size(horizontal_sum, 2);
+    % plot (x, horizontal_sum);
+    % hold on;
+    % plot ([horizontal_start_index, horizontal_stop_index], [horizontal_sum(horizontal_start_index), horizontal_sum(horizontal_stop_index)],'r^');
+    % axis tight;
+    % ylabel('Event Intensity');
+    % xlabel('Pixel Coordinate');
+    % title('Distribution of event intensity - horizontal sum');
+    % hold off;
 
-% Find the start_index and end_index of horizontal_sum
+    % Find the start_index and end_index of horizontal_sum
 
-peak_threshold = 2000;
-vertical_start_index = 1;
-for i=1:length(vertical_sum)-1; % Loop through vector intensityOverTime - 1 to avoid accessing past the vector
-    difference=abs(vertical_sum(i+1)-vertical_sum(i)); % calculate difference between neighboring intensities
-    if difference > peak_threshold% If statement to determine start time index
-        vertical_start_index=i;
-        break;
+    peak_threshold = 2000;
+    vertical_start_index = 1;
+    for i=1:length(vertical_sum)-1; % Loop through vector intensityOverTime - 1 to avoid accessing past the vector
+        difference=abs(vertical_sum(i+1)-vertical_sum(i)); % calculate difference between neighboring intensities
+        if difference > peak_threshold% If statement to determine start time index
+            vertical_start_index=i;
+            break;
+        end
     end
-end
 
-peak_threshold_rev = 500;
-vertical_stop_index = 1;
+    peak_threshold_rev = 500;
+    vertical_stop_index = 1;
 
-for i=length(vertical_sum):-1:2; % Loop through vector intensityOverTime - 1 to avoid accessing past the vector
-    difference = abs(vertical_sum(i)-vertical_sum(i-1)) % calculate difference between neighboring intensities
-    if difference > peak_threshold_rev  % If statement to determine start time index
-        vertical_stop_index=i;
-        break;
+    for i=length(vertical_sum):-1:2; % Loop through vector intensityOverTime - 1 to avoid accessing past the vector
+        difference = abs(vertical_sum(i)-vertical_sum(i-1)) % calculate difference between neighboring intensities
+        if difference > peak_threshold_rev  % If statement to determine start time index
+            vertical_stop_index=i;
+            break;
+        end
     end
+
+    % subplot(1,2,2);
+    % x = 1:size(vertical_sum, 1);
+    % plot (x, vertical_sum);
+    % hold on;
+    % plot ([vertical_start_index, vertical_stop_index], [vertical_sum(vertical_start_index), vertical_sum(vertical_stop_index)],'r^');
+    % axis tight;
+    % ylabel('Event Intensity');
+    % xlabel('Pixel Coordinate');
+    % title('Distribution of event intensity - vertical sum');
+    % hold off;
+
+    % Plot the box around the person
+
+    vertical_start_index;
+    vertical_stop_index;
+    horizontal_start_index;
+    horizontal_stop_index;
+
+    box_width = horizontal_stop_index - horizontal_start_index;
+    box_height = vertical_stop_index - vertical_start_index;
+    start_x = horizontal_start_index;
+    start_y = vertical_start_index;
+
+    % figure;
+    imshow(binaryImage, []);
+    hold on;
+    rectangle('Position',[start_x start_y box_width box_height], 'EdgeColor','r',...
+        'LineWidth',3);
+
+    I = getframe(gcf);
+    box_filename = [sprintf('%03d',itera) '.jpg'];
+    box_fullname = fullfile('test_data','box',box_filename);
+    imwrite(I.cdata,box_fullname);
+
 end
-
-subplot(1,2,2);
-x = 1:size(vertical_sum, 1);
-plot (x, vertical_sum);
-hold on;
-plot ([vertical_start_index, vertical_stop_index], [vertical_sum(vertical_start_index), vertical_sum(vertical_stop_index)],'r^');
-axis tight;
-ylabel('Event Intensity');
-xlabel('Pixel Coordinate');
-title('Distribution of event intensity - vertical sum');
-hold off;
-
-%% Plot the box around the person
-
-vertical_start_index;
-vertical_stop_index;
-horizontal_start_index;
-horizontal_stop_index;
-
-box_width = horizontal_stop_index - horizontal_start_index;
-box_height = vertical_stop_index - vertical_start_index;
-start_x = horizontal_start_index;
-start_y = vertical_start_index;
-
-figure;
-imshow(binaryImage, []);
-hold on;
-rectangle('Position',[start_x start_y box_width box_height], 'EdgeColor','r',...
-    'LineWidth',3);
-
-
 
 
 %% graph the white intensity over frame
@@ -307,19 +313,19 @@ imwrite( frm.cdata, 'savedFileName.png' ); %// save to file
 
 inputRGBVideo = VideoReader('test_data/WALK.MP4');
 
-imageNames = dir(fullfile('test_data','diff_mf_threshold2','*.jpg'));
+imageNames = dir(fullfile('test_data','box','*.jpg'));
 imageNames = {imageNames.name}';
 
-outputVideo = VideoWriter(fullfile('test_data/output','diff_mf_threshold2.avi'));
+outputVideo = VideoWriter(fullfile('test_data/output','box.avi'));
 outputVideo.FrameRate = inputRGBVideo.FrameRate;
 open(outputVideo)
 
 %for i = 1:length(imageNames)
-for i = 1:1200
+for i = 1:1088
 
    imageIndexName = [sprintf('%03d',i) '.jpg'];
    %img = imread(fullfile('test_data','yuv_images',imageNames{i}));
-   img = imread(fullfile('test_data','diff_mf_threshold2',imageIndexName));
+   img = imread(fullfile('test_data','box',imageIndexName));
    writeVideo(outputVideo,img);
 end
 
