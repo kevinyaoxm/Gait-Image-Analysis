04ceb2b35cc0e2145c0fda8da58e10a320158007
kevinyao1212 <kevinyao1212@gmail.com>
2016-07-22 13:17:38 -0700

    findGap

--- findGap.m
+++ findGap.m
@@ -3,17 +3,25 @@ function [ gapIndex ] = findGap( horizontal_sum_image )
 %   Detailed explanation goes here
 
 counter = 0;
+zeroCounter = 0;
+gapIndex = 1920;
 
-for index = 1920:1
-    if( horizontal_sum_image(index) ~= 0 )
+for index = 1920:-1:1
+    if( horizontal_sum_image(index) > 250 )
         counter = 1;
     end
     
     if( counter == 1 && horizontal_sum_image(index) == 0 )
+        zeroCounter = zeroCounter + 1;
+    end
+    
+    if( zeroCounter == 10 )
         gapIndex = index;
         break
     end
 end
+
+gapIndex
 
 end
 
