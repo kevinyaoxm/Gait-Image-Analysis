32410e17ce65f005ab5aae227ccb895ce709ab03
AlexOuyang <ouyangchenxing@hotmail.com>
2016-10-14 04:11:53 -0700

    added valid file check

--- SitStandAnalysisMain.m
+++ SitStandAnalysisMain.m
@@ -22,19 +22,22 @@ for i = 1:length(fileIndex)
     videoFileName = files(fileIndex(i)).name; 
     parsed = strsplit(videoFileName, '.');
     patientID = char(parsed(1));
-    path_rgb_video = strcat(strcat(path_videos_dir, '/'), videoFileName);
-    path_patient_save_dir = strcat(strcat(path_save_dir, '/'), patientID);
     
-    disp(strcat('======Processing pratient with ID: ', patientID)); % Display info
-    
-    % Save each analyzed sitStand event intensity to the data map
-%     try
-    savedDataMap(patientID) = AnalyzeSitStandRGB( path_rgb_video, path_patient_save_dir );
-    % Just to be safe, save the data to file
-    save('sitStandProcessedDataMap.mat' ,'savedDataMap');
-%     catch
-%         error(strcat('Error occurred when processing: ', videoFileName));
-%     end
+    if isempty(patientID) == false % Make srue the file is valid
+        path_rgb_video = strcat(strcat(path_videos_dir, '/'), videoFileName);
+        path_patient_save_dir = strcat(strcat(path_save_dir, '/'), patientID);
+
+        disp(strcat('======Processing pratient with ID: ', patientID)); % Display info
+
+        % Save each analyzed sitStand event intensity to the data map
+    %     try
+        savedDataMap(patientID) = AnalyzeSitStandRGB( path_rgb_video, path_patient_save_dir );
+        % Just to be safe, save the data to file
+        save('sitStandProcessedDataMap.mat' ,'savedDataMap');
+    %     catch
+    %         error(strcat('Error occurred when processing: ', videoFileName));
+    %     end
+    end
 end
 
 
